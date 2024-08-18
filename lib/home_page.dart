e0a26df14e4326775c1518d1c9ddd1aae3d08c6c
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-08-18 23:21:22 +0545

    create separate page to manipulate state data and view change in home page using same instance counterCubit using BlocProvider

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_counter_app/cubit/counter_cubit.dart';
+import 'package:bloc_counter_app/increment_decrement_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,6 +18,14 @@ class _MyHomePageState extends State<MyHomePage> {
     final counterCubit = BlocProvider.of<CounterCubit>(context);
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const IncrementDecrementPage()));
+              },
+              child: Icon(Icons.next_plan))
+        ],
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
@@ -38,25 +47,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 }),
           ],
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              counterCubit.decrement();
-            },
-            tooltip: 'decrement',
-            child: const Icon(Icons.minimize),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              counterCubit.increment();
-            },
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (_) => CounterCubit(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
