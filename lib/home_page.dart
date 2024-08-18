364ed68710379974cdfdc5ba49f4844206497202
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-08-18 18:59:35 +0545

    for optimization only wrap text that need to change with state inside blocBuilder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,38 +16,38 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CounterCubit, int>(
-        bloc:
-            counterCubit, //it gives us value of counter based on counter cubit
-        builder: (context, counter) {
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              title: Text(widget.title),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
+            BlocBuilder<CounterCubit, int>(
+                bloc:
+                    counterCubit, //it gives us value of counter based on counter cubit
+                builder: (context, counter) {
+                  return Text(
                     '$counter',
                     style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                ],
-              ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                counterCubit.increment();
-                setState(() {});
-              },
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ),
-          );
-        });
+                  );
+                })
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counterCubit.increment();
+          setState(() {});
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
