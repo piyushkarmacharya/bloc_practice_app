767ac0ee94cbecd6607309320fa4b46e82ac394b
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-08-26 11:34:20 +0545

    chore: add parameters in AuthLoginRequest event

--- lib/login_using_bloc/bloc/auth_event.dart
+++ lib/login_using_bloc/bloc/auth_event.dart
@@ -3,4 +3,8 @@ part of 'auth_bloc.dart';
 @immutable
 sealed class AuthEvent {}
 
-final class AuthLoginRequest extends AuthEvent {}
+final class AuthLoginRequest extends AuthEvent {
+  final String email;
+  final String password;
+  AuthLoginRequest({required this.email, required this.password});
+}
