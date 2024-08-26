f1cd7ba7fffa80a88b4b22e99661ddc27732ee32
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-08-26 17:10:52 +0545

    chore: separate logics from auth_bloc constructors and create separate method for them

--- lib/login_using_bloc/bloc/auth_bloc.dart
+++ lib/login_using_bloc/bloc/auth_bloc.dart
@@ -7,40 +7,42 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginRequest>((event, emit) async {
-      emit(AuthLoading());
-      //donot keep return as we need to run below codes after this
-      try {
-        final email = event.email;
-        final password = event.password;
-        final regEmail =
-            RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
-        if (!regEmail.hasMatch(email)) {
-          emit(AuthFail("Invalid Email"));
-          return;
-        } else if (password.length < 6) {
-          emit(AuthFail("Short Password"));
-          return;
-        }
+    on<AuthLoginRequest>(_onAuthLoginRequested);
+    on<AuthLogoutRequest>(_onAuthLogoutRequested);
+  }
+  void _onAuthLoginRequested(event, emit) async {
+    emit(AuthLoading());
+    //donot keep return as we need to run below codes after this
+    try {
+      final email = event.email;
+      final password = event.password;
+      final regEmail =
+          RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+      if (!regEmail.hasMatch(email)) {
+        emit(AuthFail("Invalid Email"));
+        return;
+      } else if (password.length < 6) {
+        emit(AuthFail("Short Password"));
+        return;
+      }
 
-        //this is where api is called
-        await Future.delayed(const Duration(seconds: 2), () {
-          return emit(
-              AuthSuccess(usr: User(name: email, uid: 11, token: "aaa")));
-        });
-      } catch (e) {
-        emit(AuthFail(e.toString()));
-      }
-    });
-    on<AuthLogoutRequest>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        await Future.delayed(const Duration(seconds: 2), () {
-          return emit(AuthInitial());
-        });
-      } catch (e) {
-        emit(AuthFail(e.toString()));
-      }
-    });
+      //this is where api is called
+      await Future.delayed(const Duration(seconds: 2), () {
+        return emit(AuthSuccess(usr: User(name: email, uid: 11, token: "aaa")));
+      });
+    } catch (e) {
+      emit(AuthFail(e.toString()));
+    }
+  }
+
+  void _onAuthLogoutRequested(event, emit) async {
+    emit(AuthLoading());
+    try {
+      await Future.delayed(const Duration(seconds: 2), () {
+        return emit(AuthInitial());
+      });
+    } catch (e) {
+      emit(AuthFail(e.toString()));
+    }
   }
 }
